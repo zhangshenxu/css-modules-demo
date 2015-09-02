74d245919c8b4dc907a6c6e261fec903c2784f34
zhangshenxu <zhangshenxu1991@gmail.com>
2015-09-02 16:36:23 +0800

    add demo

--- src/components/App.js
+++ src/components/App.js
@@ -3,6 +3,7 @@ import styles from './App.css';
 import React, { Component } from 'react';
 
 import Button from './Button/Button';
+import Demo from './Demo/Demo';
 
 export default class App extends Component {
 
@@ -10,7 +11,11 @@ export default class App extends Component {
     return (
       <div className={styles.app}>
         <h1>CSS Modules Demo</h1>
+        <p className="text">button demo</p>
         <Button style='success'/>
+        <Button style='warning'/>
+        <Button />
+        <Demo />
       </div>
       
     );
--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -6,15 +6,19 @@ export default class Button extends Component {
 
   render() {
     let className, text = 'Button';
-    if(this.props.style == 'warning') {
-      className = styles.warning;
-      text = 'warning';
-    }else if(this.props.style == 'success') {
-      className = styles.success;
-      text = 'success';
-    }else {
-      className = styles.primary;
+    switch (this.props.style) {
+      case 'warning':
+        className = styles.warning;
+        text = 'Warning';
+        break;
+      case 'success':
+        className = styles.success;
+        text = 'Success';
+        break;
+      default:
+        className = styles.primary;
     }
+
     return <button className={className} type='button'>{text}</button>;
   }
 
